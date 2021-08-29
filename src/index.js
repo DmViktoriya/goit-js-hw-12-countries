feef0a04073b1b3d5a3f438aebc3643cb6e295ce
DmViktoriya <dmytrenkoviktoriya@gmail.com>
2021-08-29 23:39:17 +0300

    Added style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './sass/main.scss';
+import fetchCountries from './js/fetchCountries';
+import { refs } from './js/refs';
+import  dataProcessing from './js/dataProcessing';
+import markup from './template/markup.hbs';
+
+refs.input.addEventListener('input', _.debounce(fetchCountries, 500));
